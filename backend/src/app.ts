0427f0e216e9541ac4b9598445bbbd3cf13ad6fd
egemrai <egeaydinoglu911@hotmail.com>
2025-07-19 10:29:15 +0300

    deploy öncesi son18

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -35,7 +35,7 @@ export const Session = session({
         maxAge: 720 * 60 * 60 * 1000,
         httpOnly: true,
         secure: true, //process.env.NODE_ENV === "production",          //secure production ise true olması lazım,
-        sameSite: "none" //process.env.NODE_ENV === "production" ? "none" : "lax"      //sameSite production ise bunun da none olması lazım
+        sameSite: 'none' //process.env.NODE_ENV === "production" ? "none" : "lax"      //sameSite production ise bunun da none olması lazım
     },
     rolling: true,              //her get,post,vs. requestlerde(sayfa yenileme gibi) session süresini yeniliyor
     store: MongoStore.create({
