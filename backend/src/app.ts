5d3e69d790df880ed65e84347b0d35aa6f0c92f0
egemrai <egeaydinoglu911@hotmail.com>
2025-11-04 18:30:43 +0300

    test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -36,8 +36,8 @@ export const Session = session({
     cookie: {
         maxAge: 720 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: env.EGE === "production",//process.env.NODE_ENV === "production",          //secure production ise true olması lazım,
-        sameSite: env.EGE === "production" ? "none" : "lax"      //sameSite production ise bunun da none olması lazım
+        secure: true, //env.EGE === "production",//process.env.NODE_ENV === "production",          //secure production ise true olması lazım,
+        sameSite: "none" //env.EGE === "production" ? "none" : "lax"      //sameSite production ise bunun da none olması lazım
     },
     rolling: true,              //her get,post,vs. requestlerde(sayfa yenileme gibi) session süresini yeniliyor
     store: MongoStore.create({
