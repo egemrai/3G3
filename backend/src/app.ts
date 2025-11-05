f24f953004f249624bab259c99b37d997cd26e32
egemrai <egeaydinoglu911@hotmail.com>
2025-11-05 10:06:21 +0300

    cors fazlalık frontend url sildim

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ app.set("trust proxy", true)
 
 //cors'u production kısmı için ihtiyaç var diye ekledim, denicem
 app.use(cors({
-  origin: ["https://docker-3g3-frontend.onrender.com",env.FRONTEND_SITE_URL,"https://threeg3.vercel.app", "http://localhost:3000"], // Frontend adresi  
+  origin: [env.FRONTEND_SITE_URL], // Frontend adresi - "https://docker-3g3-frontend.onrender.com" - "https://threeg3.vercel.app" - "http://localhost:3000"
   credentials: true
 }))
 
