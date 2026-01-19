cecf9c7fee850b3159d092ed4aa6dd71aaae17a5
egemrai <egeaydinoglu911@hotmail.com>
2026-01-19 18:54:33 +0300

    mongoose modelleri değiştirdim, discriminator type'a base modelleir ekledim lol ve valo

--- backend/src/models/offers/lol.ts
+++ backend/src/models/offers/lol.ts
@@ -5,6 +5,11 @@ const lolSchema = new Schema({
     categoryName: {type: String, required: true},
     serviceName: {type: String, required: true},
     sellerId:{type: Schema.Types.ObjectId, ref:'User', required: true},
+    title: {type: String, required: true, select: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    currency: {type: String, required: true},
+    active: {type: Boolean, required: true, default: true},
 },{ discriminatorKey: "serviceName", timestamps: true})                                                       
 
 type Lol = InferSchemaType<typeof lolSchema>
@@ -19,65 +24,47 @@ const lolAccountSchema = new Schema({
     skins: {type: Number, required: true},
     deliveryTime: {type: Number, required: true},
     stock: {type: Number, required: true},
-    title: {type: String, required: true, select: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    currency: {type: String, required: true},
-    active: {type: Boolean, required: true, default: true}, //default active eklemeyi unutma
+     //default active eklemeyi unutma
 },)                                                       
 
 type LolAccount = InferSchemaType<typeof lolAccountSchema>
-export const LolAccountModel= LolModel.discriminator<LolAccount>("LolAccount", lolAccountSchema)
+export const LolAccountModel= LolModel.discriminator<LolAccount & Lol>("LolAccount", lolAccountSchema)
 
 //LOLBOOST SCHEMA
 const lolBoostSchema = new Schema({
     // sellerId:{type: Schema.Types.ObjectId, required: true},
     server: {type: String, required: true},
     desiredRank: {type: String, required: true},
-    title: {type: String, required: true, select: true},
     serviceType: {type: String, required: true, select: true},
     deliveryTime: {type: Number, required: true},
     stock: {type: Number, required: true},
     duration: {type: Number, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    currency: {type: String, required: true},
-    active: {type: Boolean, required: true, default: true}, //default active eklemeyi unutma
 },)                                                       
 
 type LolBoost = InferSchemaType<typeof lolBoostSchema>
-export const LolBoostModel= LolModel.discriminator<LolBoost>("LolBoost", lolBoostSchema)
+export const LolBoostModel= LolModel.discriminator<LolBoost & Lol>("LolBoost", lolBoostSchema)
 
 //LOLCOACH SCHEMA
 const lolCoachSchema = new Schema({
     // sellerId:{type: Schema.Types.ObjectId, required: true},
     server: {type: String, required: true},
     rank: {type: String, required: true},
-    title: {type: String, required: true, select: true},
     deliveryTime: {type: Number, required: true},
     stock: {type: Number, required: true},
     duration: {type: Number, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    currency: {type: String, required: true},
-    active: {type: Boolean, required: true, default: true}, //default active eklemeyi unutma
 },)                                                       
 
 type LolCoach = InferSchemaType<typeof lolCoachSchema>
-export const LolCoachModel= LolModel.discriminator<LolCoach>("LolCoach", lolCoachSchema)
+export const LolCoachModel= LolModel.discriminator<LolCoach & Lol>("LolCoach", lolCoachSchema)
 
 //LOLRP SCHEMA
 const lolRPSchema = new Schema({
     // sellerId:{type: Schema.Types.ObjectId, required: true},
     server: {type: String, required: true},
-    title: {type: String, required: true, select: true},
     deliveryTime: {type: Number, required: true},
     stock: {type: Number, required: true},
     description: {type: String, required: true},
-    price: {type: Number, required: true},
-    currency: {type: String, required: true},
-    active: {type: Boolean, required: true, default: true}, //default active eklemeyi unutma
 },)                                                       
 
 type LolRP = InferSchemaType<typeof lolRPSchema>
-export const LolRPModel= LolModel.discriminator<LolRP>("LolRP", lolRPSchema)
+export const LolRPModel= LolModel.discriminator<LolRP & Lol>("LolRP", lolRPSchema)
--- backend/src/models/offers/valorant.ts
+++ backend/src/models/offers/valorant.ts
@@ -5,6 +5,11 @@ const valorantSchema = new Schema({
     categoryName: {type: String, required: true},
     serviceName: {type: String, required: true},
     sellerId:{type: Schema.Types.ObjectId, ref:'User', required: true},
+    title: {type: String, required: true, select: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    currency: {type: String, required: true},
+    active: {type: Boolean, required: true, default: true},  //default active eklemeyi unutma
 },{ discriminatorKey: "serviceName", timestamps: true})                                                       
 
 type Valorant = InferSchemaType<typeof valorantSchema>
@@ -17,67 +22,47 @@ const ValorantAccountSchema = new Schema({
     rank: {type: String, required: true},
     agents: {type: Number, required: true},
     skins: {type: Number, required: true},
-    title: {type: String, required: true, select: true},
     deliveryTime: {type: Number, required: true},
     stock: {type: Number, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    currency: {type: String, required: true},
-    active: {type: Boolean, required: true, default: true},  //default active eklemeyi unutma
 },)                                                       
 
 type ValorantAccount = InferSchemaType<typeof ValorantAccountSchema>
-export const ValorantAccountModel= ValorantModel.discriminator<ValorantAccount>("ValorantAccount", ValorantAccountSchema)
+export const ValorantAccountModel= ValorantModel.discriminator<ValorantAccount & Valorant>("ValorantAccount", ValorantAccountSchema)
 
 //VALORANTBOOST SCHEMA
 const ValorantBoostSchema = new Schema({
     // sellerId:{type: Schema.Types.ObjectId, required: true},
     server: {type: String, required: true},
     desiredRank: {type: String, required: true},
-    title: {type: String, required: true, select: true},
     serviceType: {type: String, required: true, select: true},
     deliveryTime: {type: Number, required: true},
     stock: {type: Number, required: true},
     duration: {type: Number, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    currency: {type: String, required: true},
-    active: {type: Boolean, required: true, default: true}, //default active eklemeyi unutma
 },)                                                       
 
 type ValorantBoost = InferSchemaType<typeof ValorantBoostSchema>
-export const ValorantBoostModel= ValorantModel.discriminator<ValorantBoost>("ValorantBoost", ValorantBoostSchema)
+export const ValorantBoostModel= ValorantModel.discriminator<ValorantBoost & Valorant>("ValorantBoost", ValorantBoostSchema)
 
 //VALORANTCOACH SCHEMA
 const ValorantCoachSchema = new Schema({
     // sellerId:{type: Schema.Types.ObjectId, required: true},
     server: {type: String, required: true},
     rank: {type: String, required: true},
-    title: {type: String, required: true, select: true},
     deliveryTime: {type: Number, required: true},
     stock: {type: Number, required: true},
     duration: {type: Number, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    currency: {type: String, required: true},
-    active: {type: Boolean, required: true, default: true}, //default active eklemeyi unutma
 },)                                                       
 
 type ValorantCoach = InferSchemaType<typeof ValorantCoachSchema>
-export const ValorantCoachModel= ValorantModel.discriminator<ValorantCoach>("ValorantCoach", ValorantCoachSchema)
+export const ValorantCoachModel= ValorantModel.discriminator<ValorantCoach & Valorant>("ValorantCoach", ValorantCoachSchema)
 
 //VALORANTVP SCHEMA
 const ValorantVPSchema = new Schema({
     // sellerId:{type: Schema.Types.ObjectId, required: true},
     server: {type: String, required: true},
-    title: {type: String, required: true, select: true},
     deliveryTime: {type: Number, required: true},
     stock: {type: Number, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    currency: {type: String, required: true},
-    active: {type: Boolean, required: true, default: true}, //default active eklemeyi unutma
 },)                                                       
 
 type ValorantVP = InferSchemaType<typeof ValorantVPSchema>
-export const ValorantVPModel= ValorantModel.discriminator<ValorantVP>("ValorantVP", ValorantVPSchema)
+export const ValorantVPModel= ValorantModel.discriminator<ValorantVP & Valorant>("ValorantVP", ValorantVPSchema)
